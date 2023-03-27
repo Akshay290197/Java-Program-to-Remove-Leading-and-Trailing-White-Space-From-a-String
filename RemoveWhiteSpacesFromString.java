db6c28497be155059e7b86bb341a35ab4021886b
Akshay Kumar <cakshay388@gmail.com>
2023-03-27 20:39:59 +0530

    upload

---
+++ RemoveWhiteSpacesFromString.java
@@ -0,0 +1,7 @@
+public class RemoveWhiteSpacesFromString {
+    public static void main(String[] args) {
+        String str="    Hello, here we are doing practices      ";
+        System.out.println("Before removing white spaces : "+str);
+        System.out.println("Afetr removing white spaces : "+str.trim());
+    }
+}
